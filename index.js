affa5a5c9545930f8389d3ae0254af1ec140cceb
Leif Eriksen <leif.eriksen.au@gmail.com>
2022-12-06 09:51:44 +1100

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+const http = require('http');
+const util = require('util');
+
+const server = http.createServer((req, res) => {
+  console.log("received request");
+
+  res.write(util.format('%j', req.headers))
+  res.end();
+});
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(80);
+
+console.log("Running on http://0.0.0.0:80");
